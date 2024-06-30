0d4e6cf99a8f3bff68fc8cf98a91285a9c2fb282
(Al)ta <altaficial@gmail.com>
2024-06-30 12:05:37 +0800

    Quartz sync: Jun 30, 2024, 12:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -62,7 +62,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, parseTags: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "absolute", lazyLoad: true }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
