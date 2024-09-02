94f23df9a41b67dedbc7f7178c79cab18ed71170
(Al)ta <altaficial@gmail.com>
2024-09-02 19:37:16 +0800

    Quartz sync: Sep 2, 2024, 7:37 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,6 +16,7 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
